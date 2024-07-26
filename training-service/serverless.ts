48cfc6b90b17a9bf48d2e2336d4863641aba4f03
StefanKostic88 <stefankostic88@yahoo.com>
2024-07-26 08:46:35 +0200

    training-service started

--- training-service/serverless.ts
+++ training-service/serverless.ts
@@ -5,16 +5,12 @@ require("dotenv").config({
   path: path.resolve(__dirname, "../.env"),
 });
 
-import createUser from "@functions/createUser";
-import loginUser from "@functions/loginUser";
-import myAccount from "@functions/myAccount";
-import changePassword from "@functions/changePassword";
-import editProfile from "@functions/editProfile";
+import createTraining from "@functions/createTraining";
 
 const serverlessConfiguration: AWS = {
-  service: "user-service",
+  service: "training-service",
   frameworkVersion: "3",
-  plugins: ["serverless-esbuild", "serverless-offline"],
+  plugins: ["serverless-esbuild"],
   provider: {
     name: "aws",
     runtime: "nodejs20.x",
@@ -23,29 +19,16 @@ const serverlessConfiguration: AWS = {
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
+      restApiId: "lryie611ua",
+      restApiRootResourceId: "mobsudvxtg",
     },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
       NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
     },
-    iamRoleStatements: [
-      {
-        Effect: "Allow",
-        Action: [
-          "lambda:InvokeFunction",
-          "dynamodb:Scan",
-          "dynamodb:GetItem",
-          "dynamodb:Query",
-          "dynamodb:PutItem",
-          "dynamodb:UpdateItem",
-        ],
-        Resource: [process.env.USER_TABLE_ARN],
-      },
-    ],
   },
   // import the function via paths
-  functions: { createUser, loginUser, myAccount, changePassword, editProfile },
-
+  functions: { createTraining },
   package: { individually: true },
   custom: {
     esbuild: {
